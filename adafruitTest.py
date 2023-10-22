83f25f2013e3ebedee931a49144410b0fe837cc3
kairofD <kairof.dr@gmail.com>
2023-10-22 23:29:15 +0600

    Update adafruitTest.py

--- adafruitTest.py
+++ adafruitTest.py
@@ -1,16 +1,18 @@
-import board
-import busio
+import serial
 import adafruit_gps
 
 # Create a serial connection
-uart = busio.UART(board.TX, board.RX, baudrate=9600)
+uart = serial.Serial("/dev/serial0", baudrate=9600)
 
 # Create a GPS module instance
-gps = adafruit_gps.GPS(uart)
+gps = adafruit_gps.GPS(uart, debug=False)
 
-# Update GPS data
-gps.update()
+while True:
+    # Update GPS data
+    gps.update()
 
-# Print the position
-print('Latitude: {0:.6f}'.format(gps.latitude))
-print('Longitude: {0:.6f}'.format(gps.longitude))
+    # Check if there are new coordinates available
+    if gps.has_fix:
+        # Print the position
+        print('Latitude: {0:.6f}'.format(gps.latitude))
+        print('Longitude: {0:.6f}'.format(gps.longitude))
