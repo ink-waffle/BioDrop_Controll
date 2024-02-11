95ed71a2c791df3d14ce0695ca4d92b4eebfd81a
ink_waffle <kairof.dr@gmail.com>
2024-02-12 01:56:41 +0600

    gyro

--- test.py
+++ test.py
@@ -14,6 +14,14 @@ gps = adafruit_gps.GPS(uart, debug=False)
 gravity = np.array([[0],
                     [0],
                     [0]])
+filtered_acceleration = np.array([[0],
+                                  [0],
+                                  [0]])
+speed = np.array([[0],
+                  [0],
+                  [0]])
+alpha = 0.95
+
 while True:
     # Update GPS data
     gps.update()
@@ -24,14 +32,17 @@ while True:
         print('Latitude: {0:.6f}'.format(gps.latitude), 'Longitude: {0:.6f}'.format(gps.longitude))
     else:
         print("no gps fix")
-    # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
-    alpha = 0.8
+
     acc = mpu.acceleration
     acc = np.array([[acc[0]],
                     [acc[1]],
                     [acc[2]]])
     gravity = alpha * gravity + (1 - alpha) * acc
 
-    linear_acceleration = acc - gravity
-    print('X: ' + str(linear_acceleration[0, 0]) + ' Y: ' + str(linear_acceleration[1, 0]) + ' Z: ' + str(linear_acceleration[2, 0]))
-    sleep(0.05)
+    new_acc = acc - gravity
+    speed += new_acc + filtered_acceleration
+    filtered_acceleration = acc - gravity
+    # print('X: ' + str(linear_acceleration[0, 0]) + ' Y: ' + str(linear_acceleration[1, 0]) + ' Z: ' + str(linear_acceleration[2, 0]))
+    # print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
+    print((f'\rdX:%.2f, dY: %.2f, dZ: %.2f m/s^2' % filtered_acceleration) + (f'\rX:%.2f, Y: %.2f, Z: %.2f m/s' % speed),  end='')
+    sleep(0.01)
