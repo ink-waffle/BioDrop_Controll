a276d88b292c5d5906b3bfc5074b5835608b1303
ink_waffle <kairof.dr@gmail.com>
2024-03-08 20:32:52 +0500

    gyro

--- adafruitTest.py
+++ adafruitTest.py
@@ -2,6 +2,7 @@ import serial
 import adafruit_gps
 from time import sleep
 import board
+import sys
 import adafruit_mpu6050
 
 # Create a serial connection
@@ -23,4 +24,8 @@ while True:
         print("no gps fix")
     print("Acceleration: X:%.2f, Y: %.2f, Z: %.2f m/s^2" % (mpu.acceleration))
     print("Gyro X:%.2f, Y: %.2f, Z: %.2f rad/s" % (mpu.gyro))
+    print_acceleration = round(mpu.acceleration, 2)
+    print_gyro = round(mpu.gyro, 2)
+    sys.stdout.write(f'\raX: {print_acceleration[0]}, aY: {print_acceleration[1]}, aZ: {print_acceleration[2]} rX: {print_gyro[0]}, rY: {print_gyro[1]}, rZ: {print_gyro[2]}')
+    sys.stdout.flush()
     sleep(0.1)
