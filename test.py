ae96c4ed942ae81e35a9eaac90af1c8232517a07
ink_waffle <kairof.dr@gmail.com>
2024-03-10 01:52:56 +0500

    rotation-inference

--- test.py
+++ test.py
@@ -13,16 +13,20 @@ i2c = board.I2C()
 mpu = adafruit_mpu6050.MPU6050(i2c)
 gps = adafruit_gps.GPS(uart, debug=False)
 
-noise_i = np.float64(0)
-noise_j = np.float64(0)
+# noise_i = np.float64(0)
+# noise_j = np.float64(0)
+noise = np.array([[0],
+                  [0],
+                  [0]], dtype=np.float64)
 gravity = np.array([[0.0],
                   [0.0],
                   [0.0]])
 
 for i in range(1000):
-    gyro_read = mpu.gyro
-    noise_i += np.float64(0.001) * gyro_read[0]
-    noise_j += np.float64(0.001) * gyro_read[1]
+    # gyro_read = mpu.gyro
+    # noise_i += np.float64(0.001) * gyro_read[0]
+    # noise_j += np.float64(0.001) * gyro_read[1]
+    noise += np.float64(0.001) * np.array(mpu.gyro).reshape((3, 1))
     gravity += np.float64(0.001) * np.array(mpu.acceleration).reshape((3, 1))
     sleep(0.001)
 
@@ -32,12 +36,12 @@ print_gravity = np.round(gravity, 2)
 gravity_magnitude = np.linalg.norm(gravity)
 gravity_normalized = gravity / gravity_magnitude
 
-# [[-sinj],        = [[G_x],
-#  [-sini * cosj], =  [G_y],
+# [[cosk * -sinj],        = [[G_x],
+#  [cosk * -sini * cosj + sinj * sink], =  [G_y],
 #  [cosi * cosj]]  =  [G_z]]
 pitch = np.float64(np.arcsin(-gravity_normalized[0, 0]))
 roll = np.float64(np.arcsin(-gravity_normalized[1, 0] / (np.cos(pitch))))
-
+yawn = np.float64(0)
 print(f'roll: {np.round(roll, 2)} pitch: {np.round(pitch, 2)}')
 print(f'gravity magnitude: {np.round(gravity_magnitude, 2)}')
 print(f'gX: {print_gravity[0,0]}, gY: {print_gravity[1,0]}, gZ: {print_gravity[2,0]};')
@@ -46,23 +50,29 @@ print(f'gX: {print_gravity[0,0]}, gY: {print_gravity[1,0]}, gZ: {print_gravity[2
 
 lasttime = perf_counter()
 while True:
-    di, dj, _ = mpu.gyro
+    # di, dj, _ = mpu.gyro
     # noise = 0.01 * drotation + 0.99 * noise
     # noise_i = np.float64(0.0001) * np.float64(di) + np.float64(0.9999) * np.float64(noise_i)
     # noise_j = np.float64(0.0001) * np.float64(dj) + np.float64(0.9999) * np.float64(noise_j)
-    di = np.float64(di) - noise_i
-    dj = np.float64(dj) - noise_j
+    dRotation = np.array(mpu.gyro).reshape((3, 1)) - noise
+    # di = np.float64(di) - noise_i
+    # dj = np.float64(dj) - noise_j
 
-    di = np.float64(0) if np.less_equal(np.abs(di), np.float64(0.01)) else di
-    dj = np.float64(0) if np.less_equal(np.abs(dj), np.float64(0.01)) else dj
+    dRotation = np.where(np.abs(dRotation) <= 0.01, 0, dRotation)
+    # di = np.float64(0) if np.less_equal(np.abs(di), np.float64(0.01)) else di
+    # dj = np.float64(0) if np.less_equal(np.abs(dj), np.float64(0.01)) else dj
 
     dT = np.float64(perf_counter() - lasttime)
-    roll += di * dT
-    pitch += dj * dT
+    # roll += di * dT
+    # pitch += dj * dT
+    dRotation *= dT
+    roll += dRotation[0, 0]
+    pitch += dRotation[1, 0]
+    yawn += dRotation[2, 0]
     lasttime = perf_counter()
 
-    gravity = gravity_magnitude * np.array([[-np.sin(pitch)],
-                                            [-np.sin(roll) * np.cos(pitch)],
+    gravity = gravity_magnitude * np.array([[np.cos(yawn) * -np.sin(pitch)],
+                                            [np.cos(yawn) * -np.sin(roll) * np.cos(pitch) + np.sin(pitch) * np.sin(yawn)],
                                             [np.cos(roll) * np.cos(pitch)]])
     print_gravity = np.round(gravity, 2)
     print_acceleration = np.round(np.array(mpu.acceleration), 2)
