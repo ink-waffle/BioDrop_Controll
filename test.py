45825483e7079e735df863f50affd463e02f87dd
ink_waffle <kairof.dr@gmail.com>
2024-03-11 02:03:07 +0500

    rotation-inference

--- test.py
+++ test.py
@@ -74,8 +74,8 @@ while True:
     traversedDistance += np.linalg.norm(speed) * dT
     # print_gravity = np.round(gravity, 2)
     # print_acceleration = np.round(np.array(mpu.acceleration), 2)
-    print_acceleration = np.round(acceleration, 2)
-    print_speed = np.round(speed)
-    sys.stdout.write(f'\raX: {print_acceleration[0, 0]}, aY: {print_acceleration[1, 0]}, aZ: {print_acceleration[2, 0]}; vX: {print_speed[0, 0]}, vY: {print_speed[1, 0]}, vZ: {print_speed[2, 0]}; roll: {np.round(roll, 2)}, pitch: {np.round(pitch, 2)}, yawn: {np.round(yawn, 2)}; dist: {np.round(traversedDistance, 2)}  ')
+    print_acceleration = np.round(acceleration * 100) / 100
+    print_speed = np.round(speed * 100) / 100
+    sys.stdout.write(f'\raX: {print_acceleration[0, 0]}, aY: {print_acceleration[1, 0]}, aZ: {print_acceleration[2, 0]}; vX: {print_speed[0, 0]}, vY: {print_speed[1, 0]}, vZ: {print_speed[2, 0]}; roll: {np.round(roll, 2)}, pitch: {np.round(pitch, 2)}, yawn: {np.round(yawn, 2)}; dist: {np.round(traversedDistance * 100) / 100}  ')
     sys.stdout.flush()
     sleep(0.01)
