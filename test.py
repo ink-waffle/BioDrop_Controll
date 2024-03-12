a16f563ed57e5e499a05f1540bcc3379ce1c2ace
ink_waffle <kairof.dr@gmail.com>
2024-03-12 17:58:31 +0500

    rotation-inference

--- test.py
+++ test.py
@@ -111,6 +111,6 @@ while True:
     print_acceleration = np.int16(acceleration * 100)
     print_speed = np.int16(speed * 100)
     # sys.stdout.write(f'\rvX: {print_speed[0]}, vY: {print_speed[1]}, vZ: {print_speed[2]}; roll: {np.round(roll, 2)}, pitch: {np.round(pitch, 2)}, yawn: {np.round(yawn, 2)}; dist: {np.int16(traversedDistance * 100)}  ')
-    sys.stdout.write(f'\rxX: {print_x[0]}, xY: {print_x[1]}, xZ: {print_x[2]}; yX: {print_y[0]}, yY: {print_y[1]}, yZ: {print_y[2]}; roll: {np.round(roll, 2)}, pitch: {np.round(pitch, 2)}, yawn: {np.round(yawn, 2)}; dist: {np.int16(traversedDistance * 100)}  ')
+    sys.stdout.write(f'\rxX: {print_x[0]}, xY: {print_x[1]}, xZ: {print_x[2]}; yX: {print_y[0]}, yY: {print_y[1]}, yZ: {print_y[2]}; ')
     sys.stdout.flush()
     sleep(0.01)
