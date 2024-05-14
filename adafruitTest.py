ad6240fa82c6d79a6d450b051f11602ac9872fc4
ink_waffle <kairof.dr@gmail.com>
2024-05-15 02:00:34 +0500

    rotation-inference

--- adafruitTest.py
+++ adafruitTest.py
@@ -18,6 +18,12 @@ noise = np.array([0,
 gravity = np.array([0,
                     0,
                     0], dtype=np.float32)
+speed = np.array([0,
+                  0,
+                  0], dtype=np.float32)
+position = np.array([0,
+                     0,
+                     0], dtype=np.float32)
 
 for i in range(1000):
     noise += np.float32(0.001) * np.array(mpu.gyro)
@@ -29,6 +35,10 @@ for i in range(1000):
 print_gravity = np.round(gravity, 2)
 gravity_magnitude = np.linalg.norm(gravity)
 gravity_normalized = gravity / gravity_magnitude
+ox = np.array([1, 0, 0], dtype=np.float32)
+ox -= np.dot(ox, gravity_normalized) * gravity_normalized
+ox /= np.linalg.norm(ox)
+oy = np.cross(ox, gravity_normalized)
 
 lasttime = np.float32(perf_counter())
 while True:
@@ -45,14 +55,29 @@ while True:
     dRotation = np.array(mpu.gyro, dtype=np.float32) - noise
     dRotation = np.where(np.less_equal(np.abs(dRotation), np.float32(0.05)), 0, dRotation)
     dT = np.float32(perf_counter()) - lasttime
-    gravity_normalized_mid = gravity_normalized + (np.cross(-dRotation, gravity_normalized) * dT / 2)
-    gravity_normalized += np.cross(-dRotation, gravity_normalized_mid) * dT
-    gravity_normalized /= np.linalg.norm(gravity_normalized)
-    gravity = gravity_normalized * gravity_magnitude
-    acceleration = np.array(mpu.acceleration)
-    lasttime = np.float32(perf_counter())
 
-    sys.stdout.write(f'\rgX_: {gravity[0]:.2f}, gY_: {gravity[1]:.2f}, gZ_: {gravity[2]:.2f} ; gX: {acceleration[0]:.2f}, gY: {acceleration[1]:.2f}, gZ: {acceleration[2]:.2f}     ')
-    # sys.stdout.write(f'\rwX: {dRotation[0]:.2f}, wY: {dRotation[1]:.2f}, wZ: {dRotation[2]:.2f}       ')
+    gravity_normalized_mid = gravity_normalized + (np.cross(-dRotation, gravity_normalized) * dT / 2)
+    ox_mid = ox + (np.cross(-dRotation, ox) * dT / 2)
+    oy_mid = ox + (np.cross(-dRotation, oy) * dT / 2)
+
+    gravity_normalized += np.cross(-dRotation, gravity_normalized_mid) * dT
+    ox += np.cross(-dRotation, ox_mid) * dT
+    oy += np.cross(-dRotation, oy) * dT
+
+    gravity_normalized /= np.linalg.norm(gravity_normalized)
+    ox /= np.linalg.norm(ox)
+    oy /= np.linalg.norm(oy)
+
+    gravity = gravity_normalized * gravity_magnitude
+    acceleration = np.array(mpu.acceleration) - gravity
+    acceleration = np.array([np.dot(acceleration, ox),
+                             np.dot(acceleration, oy),
+                             np.dot(acceleration, gravity_normalised)])
+    speed += acceleration * dT
+    position += speed * dT
+
+    lasttime = np.float32(perf_counter())
+    # sys.stdout.write(f'\rgX_: {gravity[0]:.2f}, gY_: {gravity[1]:.2f}, gZ_: {gravity[2]:.2f} ; gX: {acceleration[0]:.2f}, gY: {acceleration[1]:.2f}, gZ: {acceleration[2]:.2f}     ')
+    sys.stdout.write(f'\raX: {acceleration[0]:.2f}, aY: {acceleration[1]:.2f}, aZ: {acceleration[2]:.2f} ; vX: {speed[0]:.2f}, vY: {speed[1]:.2f}, zZ: {speed[2]:.2f} ; pX: {position[0]:.2f}, pY: {position[1]:.2f}, pZ: {position[2]:.2f}       ')
     sys.stdout.flush()
     sleep(0.01)
