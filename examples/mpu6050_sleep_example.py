16e820dc783ca531f9ceca1e48004b80a7c57cb5
ink_waffle <kairof.dr@gmail.com>
2024-05-24 02:40:02 +0500

    upd

--- examples/mpu6050_sleep_example.py
+++ examples/mpu6050_sleep_example.py
@@ -20,7 +20,7 @@ lastacc = np.array([0, 0, 0], dtype=np.float32)
 time = np.float32(0)
 for _ in range(10000):
     acc = np.array(mpu.acceleration, dtype=np.float32)
-    print(acc)
+    print(acc.shape)
     if np.equal(acc, lastacc):
         print(f"Same Acceleration detected: {acc} and {lastacc}")
         break
