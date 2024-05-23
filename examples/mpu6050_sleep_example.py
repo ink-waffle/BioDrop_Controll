3ad31a0f7e37db41fa9116148ed5ccc37528103c
ink_waffle <kairof.dr@gmail.com>
2024-05-24 03:08:20 +0500

    upd

--- examples/mpu6050_sleep_example.py
+++ examples/mpu6050_sleep_example.py
@@ -1,63 +1,63 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-import time
-import board
-import adafruit_mpu6050
+import datetime
+import serial
+import adafruit_gps
 from time import sleep, perf_counter
+import board
+import sys
+import adafruit_mpu6050
 import numpy as np
+import sys
+import pandas as pd
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+i2c = board.I2C()
 mpu = adafruit_mpu6050.MPU6050(i2c)
-mpu.clock_source = adafruit_mpu6050.ClockSource.CLKSEL_INTERNAL_8MHz
-# This example is meant to be used with the serial plotter which makes
-# it easier to see how the readings change with different settings.
-# Make sure to poke and prod the sensor while the demo is running to
-# generate some interesting data!
-lasttime = perf_counter()
-lastacc = np.array([0, 0, 0], dtype=np.float32)
-time = np.float32(0)
-for _ in range(1000):
-    acc = np.array(mpu.acceleration, dtype=np.float32)
-    if np.array_equal(acc, lastacc):
-        print(f"Same Acceleration detected: {acc} and {lastacc}")
-        break
-    lastacc = acc
-    time += perf_counter() - lasttime
-    lasttime = perf_counter()
+
+noise = np.array([0,
+                  0,
+                  0], dtype=np.float32)
+
+for i in range(40):
+    noise += np.float32(0.025) * np.array(mpu.gyro)
+    sleep(0.025)
+
+
+
+debug_df = []
+
+lasttime = np.float32(perf_counter())
+try:
+    while True:
+        input_gyro = mpu.gyro
+        dT = np.float32(perf_counter()) - lasttime
+        lasttime = np.float32(perf_counter())
+        
+        # dRotation = np.array(mpu.gyro, dtype=np.float32) - noise
+        # dRotation = np.where(np.less_equal(np.abs(dRotation), np.float32(0.01)), 0, dRotation)
+        
+
+        data = {'GyroX': input_gyro[0], 'GyroY': input_gyro[1], 'GyroZ': input_gyro[2],
+                'NoiseX': noise[0], 'NoiseY': noise[1], 'NoiseZ': noise[2],
+                'dT': dT
+                }
+        debug_df.append(data)
+
+        # sys.stdout.write(f'\raX: {acceleration[0]:.2f}, aY: {acceleration[1]:.2f}, aZ: {acceleration[2]:.2f} ; vX: {speed[0]:.2f}, vY: {speed[1]:.2f}, zZ: {speed[2]:.2f} ; pX: {position[0]:.2f}, pY: {position[1]:.2f}, pZ: {position[2]:.2f}       ')
+        sys.stdout.flush()
+        sleep(0.01)
+
+except KeyboardInterrupt:
+    # Graceful exit on interrupt
+    print("KeyboardInterrupt detected. Saving data and exiting...")
     
-print(f"Average time was: {time/1000}")
-# while True:
-#     # first show some 'normal' readings
-#     print('Normal:\n')
-#     mpu.reset()
-#     time.sleep(0.1)
-#     for count in range(0, 40):
-#         print(mpu.acceleration)
-#         time.sleep(0.1)
+finally:
+    # Convert list of telemetry data to a pandas DataFrame
+    df = pd.DataFrame(debug_df)
 
-#     print('Cycle:\n')
-#     # Next, set a slow cycle rate so the effect can be seen clearly.
-#     mpu.cycle_Rate = adafruit_mpu6050.Rate.CYCLE_5_HZ
-#     # ensure that we're not sleeping or cycle won't work
-#     mpu.sleep = False
-#     # Finally, enable cycle mode
-#     mpu.cycle = True
+    # Get the current time for the filename
+    current_time = datetime.datetime.now().strftime("%m%d_%H%M%S")
 
-#     for count in range(0, 40):
-#         print(mpu.acceleration)
-#         time.sleep(0.1)
+    # Export DataFrame to a CSV file with the current time in the filename
+    filename = f"logs/accelerometerlogs/gyroTest_{current_time}.csv"
+    df.to_csv(filename, index=False)
 
-#     print('Sleep:\n')
-#     # Finally enable sleep mode. Note that while we can still fetch
-#     #  data from the measurement registers, the measurements are not
-#     #  updated. In sleep mode the accelerometer and gyroscope are
-#     #  deactivated to save power, so measurements are halted.
-
-#     mpu.cycle = False
-#     mpu.sleep = True
-
-#     for count in range(0, 40):
-#         print(mpu.acceleration)
-#         time.sleep(0.1)
+    print(f"Telemetry data saved to {filename}")
