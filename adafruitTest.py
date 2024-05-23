279c363fe189abc136f14a922b0020c8e9dee258
ink_waffle <kairof.dr@gmail.com>
2024-05-24 03:24:05 +0500

    upd

--- adafruitTest.py
+++ adafruitTest.py
@@ -71,7 +71,7 @@ try:
         dT = np.float32(perf_counter()) - lasttime
         lasttime = np.float32(perf_counter())
         
-        dRotation = np.array(mpu.gyro, dtype=np.float32) - noise
+        dRotation = np.array(input_gyro, dtype=np.float32) - noise
         dRotation = np.where(np.less_equal(np.abs(dRotation), np.float32(0.02)), 0, dRotation)
         
         gravity_normalized_mid = gravity_normalized + (np.cross(-dRotation, gravity_normalized) * dT / 2)
@@ -98,8 +98,7 @@ try:
         # speed -= np.float32(0.001) * speed / np.linalg.norm(speed)
         position += np.where(np.less_equal(np.abs(speed), np.float32(0.2)), 0, speed) * dT
 
-        data = {'GyroX': input_gyro[0], 'GyroY': input_gyro[1], 'GyroZ': input_gyro[2],
-                'NoiseX': noise[0], 'NoiseY': noise[1], 'NoiseZ': noise[2],
+        data = {'GyroX': dRotation[0], 'GyroY': dRotation[1], 'GyroZ': dRotation[2],
                 'ExpGravityX': gravity_normalized[0], 'ExpGravityY': gravity_normalized[1],  'ExpGravityZ': gravity_normalized[2],
                 'ExpOxX': ox[0], 'ExpOxY': ox[1], 'ExpOxZ': ox[2],
                 'ExpOyX': oy[0], 'ExpOyY': oy[1], 'ExpOyZ': oy[2],
@@ -109,8 +108,8 @@ try:
                 }
         debug_df.append(data)
 
-        sys.stdout.write(f'\rgX: {gravity[0]:.2f}, gY: {gravity[1]:.2f}, gZ: {gravity[2]:.2f} ; aX: {acceleration[0]:.2f}, aY: {acceleration[1]:.2f}, aZ: {acceleration[2]:.2f}     ')
-        # sys.stdout.write(f'\raX: {acceleration[0]:.2f}, aY: {acceleration[1]:.2f}, aZ: {acceleration[2]:.2f} ; vX: {speed[0]:.2f}, vY: {speed[1]:.2f}, zZ: {speed[2]:.2f} ; pX: {position[0]:.2f}, pY: {position[1]:.2f}, pZ: {position[2]:.2f}       ')
+        # sys.stdout.write(f'\rgX: {gravity[0]:.2f}, gY: {gravity[1]:.2f}, gZ: {gravity[2]:.2f} ; aX: {acceleration[0]:.2f}, aY: {acceleration[1]:.2f}, aZ: {acceleration[2]:.2f}     ')
+        sys.stdout.write(f'\raX: {acceleration[0]:.2f}, aY: {acceleration[1]:.2f}, aZ: {acceleration[2]:.2f} ; vX: {speed[0]:.2f}, vY: {speed[1]:.2f}, vZ: {speed[2]:.2f} ; pX: {position[0]:.2f}, pY: {position[1]:.2f}, pZ: {position[2]:.2f}       ')
         sys.stdout.flush()
         sleep(0.01)
 
