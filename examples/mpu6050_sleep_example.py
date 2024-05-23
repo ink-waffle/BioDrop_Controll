1571d6af2cb97134bea7f2a3e63ddb1970be1630
ink_waffle <kairof.dr@gmail.com>
2024-05-24 02:24:48 +0500

    upd

--- examples/mpu6050_sleep_example.py
+++ examples/mpu6050_sleep_example.py
@@ -9,6 +9,9 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 mpu = adafruit_mpu6050.MPU6050(i2c)
 print(mpu.cycle)
+for count in range(0, 40):
+    print(mpu.acceleration)
+    time.sleep(0.1)
 mpu.clock_source = adafruit_mpu6050.ClockSource.CLKSEL_INTERNAL_8MHz
 # This example is meant to be used with the serial plotter which makes
 # it easier to see how the readings change with different settings.
